3b65f19318e621f30c2cc99dd30bdba638e83e80
XeonCore <xeoncore@users.noreply.github.com>
2014-06-04 17:08:01 +1000

    Added Torrent streaming source

--- streamers/torrent.js
+++ streamers/torrent.js
@@ -1,15 +1,35 @@
-var inherits = require('util').inherits;
+var inherits = require('util').inherits
+  , mime = require('mime')
+  , torrentStream = require('torrent-stream')
+  , rangeParser = require('range-parser');
 
 var Streamer = require('./base');
 
 /* -- Torrent Streamer -- */
-function TorrentStreamer() {
+function TorrentStreamer(source, options) {
 	if(!(this instanceof TorrentStreamer)) 
-		return new TorrentStreamer();
+		return new TorrentStreamer(source, options);
 
-	Streamer.call(this);
+	Streamer.call(this, options);
+	var self = this;
 
+	this._torrentStream = torrentStream(source, options.torrent);
+	this._torrentStream.on('uninterested', function() { self._torrentStream.swarm.pause() });
+	this._torrentStream.on('interested',   function() { self._torrentStream.swarm.resume() });
 
+	this._torrentStream.on('ready', function() {
+		if (typeof options.fileIndex !== 'number') {
+			index = self._torrentStream.files.reduce(function(a, b) {
+				return a.length > b.length ? a : b;
+			});
+			index = self._torrentStream.files.indexOf(index);
+		}
+
+		self._torrentStream.files[index].select();
+		self.file = self._torrentStream.files[index];
+		self._progress.setLength(self.file.length);
+		self._streamify.resolve(self.file.createReadStream());
+	})
 }
 inherits(TorrentStreamer, Streamer);
 
